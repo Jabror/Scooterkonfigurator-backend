de9c8b0dcb62dec177736b472d38760e78173182
Jabror <david19.schuler@bluewin.ch>
2026-02-26 18:48:46 +0100

    Add InputValue field to order schema

--- models/Order.js
+++ models/Order.js
@@ -4,10 +4,12 @@ const orderSchema = new mongoose.Schema({
   paypalOrderId: { type: String, required: true },
   produkte: [{ type: Number }], // Preise oder später Produkt-IDs
   Produkte: [{ type: Object }], 
-  KonfigurationenArray: [{ type: String }], 
+  KonfigurationenArray: [{ type: String }],
+  InputValue: { type: String },
   total: { type: Number, required: true },
   status: { type: String, default: "PAID" },
   createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model("Order", orderSchema);
+
